aac295b9c82d7ce2d72a97b4d16e5564b4f9949c
Tyler Wilding <xTVaser@users.noreply.github.com>
2022-07-25 21:52:21 -0400

    release: add windows launcher release download link (#44)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,93 @@
 import React, { useState, useEffect, useRef } from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import * as platform from 'platform';
+
+function LauncherDownloadLink() {
+  const [loading, setLoading] = useState(true);
+  const [available, setAvailable] = useState(false);
+  const [apiError, setApiError] = useState(false);
+
+  const [downloadUrl, setDownloadUrl] = useState("#");
+  const [forPlatform, setForPlatform] = useState("");
+  const [launcherVersion, setLauncherVersion] = useState("");
+
+  useEffect(async () => {
+    const response = await fetch(
+      `https://api.github.com/repos/open-goal/launcher/releases/latest`
+    );
+    if (response.status != 200) {
+      setLoading(false);
+      setApiError(true);
+      return;
+    }
+    const data = await response.json();
+    // TODO - detect Windows 10+ specifically
+    const isWindows = platform.os.family.toLowerCase().includes("win");
+    if (!isWindows) {
+      setLoading(false);
+      return;
+    }
+    for (const asset of data.assets) {
+      if (asset.name.match(/^.*\.msi$/)) {
+        setAvailable(true);
+        setDownloadUrl(asset.browser_download_url);
+        setForPlatform("Windows");
+        setLauncherVersion(data.tag_name);
+        setLoading(false);
+        return;
+      }
+    }
+  }, []);
+
+  function DownloadContent() {
+    if (loading) {
+      return (
+        <div className="text">
+          <h3 className="title">Download</h3>
+          <p className="description">Fetching latest release...</p>
+        </div>
+      );
+    }
+    if (apiError) {
+      return (
+        <div className="text">
+          <h3 className="title">Download</h3>
+          <p className="description">Can't fetch latest release, API error or you are rate-limited!</p>
+        </div>
+      );
+    }
+    if (!available) {
+      return (
+        <div className="text">
+          <h3 className="title">Download</h3>
+          <p className="description">Everything you need to start playing with a copy of your original game</p>
+          <span className="more">Coming Soon, Launcher only supports Windows!&nbsp;&nbsp;<svg width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M7.844 12.016c.199 0 .375-.07.527-.211l5.203-5.01a.728.728 0 00.246-.554.728.728 0 00-.246-.553L8.406.694a.84.84 0 00-.562-.21c-.211 0-.39.078-.536.237a.79.79 0 00-.22.553c0 .211.082.393.246.545l3.797 3.657H1.04a.765.765 0 00-.571.246.748.748 0 00-.22.58c.012.199.094.369.246.51.152.14.328.21.527.21h10.073L7.299 10.68a.776.776 0 00-.237.545.714.714 0 00.22.545c.152.164.34.246.562.246z" fill="#febb01" fillRule="nonzero"></path></svg></span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="text">
+          <h3 className="title">Download</h3>
+          <p className="description">Everything you need to start playing with a copy of your original game</p>
+          <span className="more">Launcher {launcherVersion} for {forPlatform}&nbsp;&nbsp;<svg width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M7.844 12.016c.199 0 .375-.07.527-.211l5.203-5.01a.728.728 0 00.246-.554.728.728 0 00-.246-.553L8.406.694a.84.84 0 00-.562-.21c-.211 0-.39.078-.536.237a.79.79 0 00-.22.553c0 .211.082.393.246.545l3.797 3.657H1.04a.765.765 0 00-.571.246.748.748 0 00-.22.58c.012.199.094.369.246.51.152.14.328.21.527.21h10.073L7.299 10.68a.776.776 0 00-.237.545.714.714 0 00.22.545c.152.164.34.246.562.246z" fill="#febb01" fillRule="nonzero"></path></svg></span>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className={`box ${!available ? "disabled" : ""}`}>
+      <span className="icon">
+        <img src="/img/download.svg" />
+      </span>
+      <DownloadContent />
+      <a href={downloadUrl} className="link">Read More</a>
+    </div>
+  );
+}
 
 function HomepageHeader() {
   let textToType = `(format 0 "~S~%" (-> *site-config* tag-line))`;
@@ -56,17 +140,7 @@ function HomepageHeader() {
             </div>
           </div>
           <div className="col col--4">
-            <div className="box disabled">
-              <span className="icon">
-                <img src="/img/download.svg" />
-              </span>
-              <div className="text">
-                <h3 className="title">Download</h3>
-                <p className="description">Everything you need to start playing with a copy of your original game</p>
-                <span className="more">Coming Soon!&nbsp;&nbsp;<svg width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M7.844 12.016c.199 0 .375-.07.527-.211l5.203-5.01a.728.728 0 00.246-.554.728.728 0 00-.246-.553L8.406.694a.84.84 0 00-.562-.21c-.211 0-.39.078-.536.237a.79.79 0 00-.22.553c0 .211.082.393.246.545l3.797 3.657H1.04a.765.765 0 00-.571.246.748.748 0 00-.22.58c.012.199.094.369.246.51.152.14.328.21.527.21h10.073L7.299 10.68a.776.776 0 00-.237.545.714.714 0 00.22.545c.152.164.34.246.562.246z" fill="#febb01" fill-rule="nonzero"></path></svg></span>
-              </div>
-              <a href="#" className="link">Read More</a>
-            </div>
+            <LauncherDownloadLink />
           </div>
         </div>
       </div>
@@ -109,7 +183,7 @@ export default function Home() {
               <div className="col col--6">
                 <h2 className="monospaced">Watch our Progress</h2>
                 <p>OpenGOAL is always improving, if you want to see the latest changes checkout the progress page</p>
-                <a href="/progress">Check it Out</a>
+                <a href="/progress/milestones">Check it Out</a>
               </div>
               <div className="col col--6">
                 <h2 className="monospaced">Read the Docs</h2>
