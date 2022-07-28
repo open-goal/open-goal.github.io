601647297bf17af3c6872c137acfe49840c2ef58
Tyler Wilding <xtvaser@gmail.com>
2022-07-28 17:34:30 -0400

    ci: remove static content folder, deploy with new beta feature

--- docs/assets/js/0379b505.003d411b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9891],{15745:function(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- docs/assets/js/6317c8d9.3a0e5ed0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1758],{83769:function(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
