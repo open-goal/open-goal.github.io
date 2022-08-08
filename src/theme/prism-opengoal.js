55459168d0bb0e5922d41189c666494d370e065b
Tyler Wilding <xTVaser@users.noreply.github.com>
2022-08-07 21:37:14 -0400

    theme: rough-port of the textmate grammars to prism.js (#59)

---
+++ src/theme/prism-opengoal.js
@@ -0,0 +1,82 @@
+// TODO - quoted expressions
+// TODO - proper sexpr
+Prism.languages.opengoal = {
+	'comment': [
+		{
+			pattern: /#\|[\s\S]*?(?:\|#|$)/,
+			lookbehind: true,
+			greedy: true
+		},
+		{
+			pattern: /(^|[^\\:]);+.*/,
+			lookbehind: true,
+			greedy: true
+		}
+	],
+  'string': {
+		pattern: /"(?:[^"\\]|\\.)*"/,
+		greedy: true
+	},
+  'keyword': {
+		pattern: /de[sf][\w\d._:+=><!?*-]*/,
+		lookbehind: true
+	},
+  'function': {
+		pattern: /((?:^|[^'])\()[\w*+!?'<>=/.-]+(?=[\s)]|$)/,
+		lookbehind: true
+	},
+  'constant': [
+    // nil
+    {
+      pattern: /(none)(?=\s|\)|\]|\})/,
+      greedy: true
+    }
+  ],
+  'boolean': [
+    // true/false,
+    {
+      pattern: /(#t|#f)/,
+      greedy: true
+    }
+  ],
+  'number': [
+    // ratio
+    {
+      pattern: /([-+]?\d+\/\d+)/,
+      greedy: true
+    },
+    // hex
+    {
+      pattern: /([-+]?#[0-9a-fA-F]+N?)/,
+      greedy: true
+    },
+    // binary
+    {
+      pattern: /([-+]?#b[0-9a-fA-F]+N?)/,
+      greedy: true
+    },
+    // floats
+    {
+      pattern: /([-+]?[0-9]+(?:(\.|(?=[eEM]))[0-9]*([eE][-+]?[0-9]+)?)M?)/,
+      greedy: true
+    },
+    // integers
+    {
+      pattern: /\s([-+]?\d+N?)/,
+      greedy: true
+    }
+  ],
+  'property': [
+    // keyword
+    {
+      pattern: /(\s|\(|\[|\{):[\w\#\.\-\_\:\+\=\>\<\/\!\?\*]+(?=(\s|\)|\]|\}|\,))/,
+      lookbehind: true
+    }
+  ],
+  'operator': [
+    // reader macros
+    {
+      pattern: /('|,@|`|,|&->)/
+    }
+  ]
+};
