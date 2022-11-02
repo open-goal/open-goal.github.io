d978161e6fb770473ca3cd0bab3d81ba60a7a64d
Tyler Wilding <xTVaser@users.noreply.github.com>
2022-11-01 23:31:31 -0400

    release: add linux download link (#92)

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,21 +24,33 @@ export function LauncherDownloadLink() {
       return;
     }
     const data = await response.json();
-    // TODO - detect Windows 10+ specifically
     const isWindows = platform.os.family.toLowerCase().includes("win");
-    if (!isWindows) {
+    const isLinux = platform.os.family.toLowerCase().includes("linux");
+    if (isWindows) {
+      for (const asset of data.assets) {
+        if (asset.name.match(/^.*\.msi$/)) {
+          setAvailable(true);
+          setDownloadUrl(asset.browser_download_url);
+          setForPlatform("Windows");
+          setLauncherVersion(data.tag_name);
+          setLoading(false);
+          return;
+        }
+      }
+    } else if (isLinux) {
+      for (const asset of data.assets) {
+        if (asset.name.match(/^.*\.AppImage$/)) {
+          setAvailable(true);
+          setDownloadUrl(asset.browser_download_url);
+          setForPlatform("Linux");
+          setLauncherVersion(data.tag_name);
+          setLoading(false);
+          return;
+        }
+      }
+    } else {
       setLoading(false);
       return;
-    }
-    for (const asset of data.assets) {
-      if (asset.name.match(/^.*\.msi$/)) {
-        setAvailable(true);
-        setDownloadUrl(asset.browser_download_url);
-        setForPlatform("Windows");
-        setLauncherVersion(data.tag_name);
-        setLoading(false);
-        return;
-      }
     }
   }, []);
 
@@ -64,7 +76,7 @@ export function LauncherDownloadLink() {
         <div className="text">
           <h3 className="title">Download</h3>
           <p className="description">Everything you need to start playing with a copy of your original game</p>
-          <span className="more">Coming Soon, Launcher only supports Windows!&nbsp;&nbsp;<svg width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M7.844 12.016c.199 0 .375-.07.527-.211l5.203-5.01a.728.728 0 00.246-.554.728.728 0 00-.246-.553L8.406.694a.84.84 0 00-.562-.21c-.211 0-.39.078-.536.237a.79.79 0 00-.22.553c0 .211.082.393.246.545l3.797 3.657H1.04a.765.765 0 00-.571.246.748.748 0 00-.22.58c.012.199.094.369.246.51.152.14.328.21.527.21h10.073L7.299 10.68a.776.776 0 00-.237.545.714.714 0 00.22.545c.152.164.34.246.562.246z" fill="#febb01" fillRule="nonzero"></path></svg></span>
+          <span className="more">Coming Soon, Launcher only supports Windows and Linux!&nbsp;&nbsp;<svg width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M7.844 12.016c.199 0 .375-.07.527-.211l5.203-5.01a.728.728 0 00.246-.554.728.728 0 00-.246-.553L8.406.694a.84.84 0 00-.562-.21c-.211 0-.39.078-.536.237a.79.79 0 00-.22.553c0 .211.082.393.246.545l3.797 3.657H1.04a.765.765 0 00-.571.246.748.748 0 00-.22.58c.012.199.094.369.246.51.152.14.328.21.527.21h10.073L7.299 10.68a.776.776 0 00-.237.545.714.714 0 00.22.545c.152.164.34.246.562.246z" fill="#febb01" fillRule="nonzero"></path></svg></span>
         </div>
       );
     } else {
