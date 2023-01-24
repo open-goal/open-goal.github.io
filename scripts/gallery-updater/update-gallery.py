978dc812f6928ab5b467f3dfedb9039d2a44ebc6
Tyler Wilding <xtvaser@gmail.com>
2023-01-23 22:06:01 -0500

    gallery: cleanup the gallery scripts

--- scripts/gallery-updater/update-gallery.py
+++ scripts/gallery-updater/update-gallery.py
@@ -3,122 +3,108 @@ import os
 from pathlib import Path
 import json
 
-galleryLinks = {
+galleryMap = {
   'jak1': {
     'name': "Jak and Daxter: The Precursor Legacy",
+    'metaTitle': "Jak and Daxter: The Precursor Legacy Galleries",
+    'metaDescription': "A collection of images taken with OpenGOAL as well as during it's development running Jak and Daxter: The Precursor Legacy",
     'galleries': {
       'promo': {
         'name': "Promo Gallery",
         'description': "Composed shots meant to show off the port, captured at 16K (15360x8640) and downscaled to 2K (2560x1440).",
-        'folder': 'jak1promo',
+        'folder': 'jak1/promo',
         'media': []
       },
       'dev': {
         'name': "Development Gallery",
-        'description': "Various pictures and videos we took while working on the Jak and Daxter port.",
-        'folder': 'jak1',
+        'description': "Various pictures and videos we took while working on the Jak and Daxter: The Precursor Legacy port.",
+        'folder': 'jak1/dev',
         'media': []
       }
     }
   },
   'jak2': {
     'name': "Jak II",
+    'metaTitle': "Jak II Galleries",
+    'metaDescription': "A collection of images taken with OpenGOAL as well as during it's development running Jak II",
     'galleries': {
       'dev': {
         'name': "Development Gallery",
         'description': "Various pictures and videos we took while working on Jak II.",
-        'folder': 'jak2',
-        'media': []
-      }
-    }
-  },
-  'jak3': {
-    'name': "Jak 3",
-    'galleries': {
-      'dev': {
-        'name': "Development Gallery",
-        'description': "Various pictures and videos we took while working on Jak II.",
-        'folder': 'jak2',
-        'media': []
-      }
-    }
-  },
-  'jakx': {
-    'name': "Jak X",
-    'galleries': {
-      'dev': {
-        'name': "Development Gallery",
-        'description': "Various pictures and videos we took while working on Jak II.",
-        'folder': 'jak2',
-        'media': []
-      }
-    }
-  },
-  'misc': {
-    'name': "Miscellaneous",
-    'galleries': {
-      'dev': {
-        'name': "Development Gallery",
-        'description': "Various pictures and videos we took while working on Jak II.",
-        'folder': 'jak2',
+        'folder': 'jak2/dev',
         'media': []
       }
     }
   }
 }
 
-def get_links(key, gallery_name, specific_order = False, no_captions = False):
-  gallery = galleryLinks[key]["galleries"][gallery_name]
-  folder_to_search = "./static/gallery/{}".format(gallery["folder"])
-  order_file = []
-  if os.path.exists("{}/order.json".format(folder_to_search)):
-    with open("{}/order.json".format(folder_to_search), 'r') as f:
-      order_file = json.load(f)
-  if os.path.isdir(folder_to_search):
-    files = glob.glob(folder_to_search + "/*.png", recursive=True)
-    files.extend(glob.glob(folder_to_search + "/*.jpg", recursive=True))
-    files.extend(glob.glob(folder_to_search + "/*.jpeg", recursive=True))
-    file_captions = {};
-    # change image captions if wanted
-    if os.path.exists("{}/captions.json".format(folder_to_search)):
-      with open("{}/captions.json".format(folder_to_search), 'r') as f:
-        file_captions = json.load(f)
-    for f in files:
-      name = Path(f).stem.split("_")[0]
-      timestamp = ""
-      if specific_order:
-        # kinda hacky, using the timestamp field this way...
-        timestamp = len(order_file) - order_file.index(name)
-      else:
-        timestamp = Path(f).stem.split("_")[1]
-      caption = (file_captions.get(name), "")[no_captions]
-      gallery["media"].append({
-        'fileName': os.path.basename(f),
-        'name': name,
-        'timestamp': timestamp,
-        'caption': (caption, name.replace("-", " ").title())[caption is None],
-        'video': False
-      })
-    # get videos potentially
-    if os.path.exists("{}/videos.json".format(folder_to_search)):
-      with open("{}/videos.json".format(folder_to_search), 'r') as f:
-        data = json.load(f)
-        for video in data:
-          gallery["media"].append({
-            'link': video["link"].replace("watch?v=", "embed/"),
-            'timestamp': video["timestamp"],
-            'caption': video["caption"],
-            'video': True
-          })
-    # sort by timestamp
-    gallery["media"].sort(key=lambda x: x["timestamp"], reverse=True)
+def is_file_in_dir(file_listing, file_name):
+  for f in file_listing:
+    name = os.path.basename(f)
+    if name == file_name:
+      return True
+  return False
 
-# generate the gallery data
-get_links('jak1', 'dev')
-get_links('jak1', 'promo', specific_order=True)
-get_links('jak2', 'dev')
 
-def generate_gallery(title, description, game_name, gallery_names, out_path):
+def init_media_links(folder, meta):
+  files = glob.glob(folder + "/*.png", recursive=False)
+  files.extend(glob.glob(folder + "/*.jpg", recursive=False))
+  files.extend(glob.glob(folder + "/*.jpeg", recursive=False))
+
+  media_links = []
+  file_names = set()
+  # If we've provided ordering via the `meta.json` file, use that to enumerate through
+  # add any extra images to the end that we havn't encountered
+  if meta is not None:
+    for file_info in meta:
+      if "name" not in file_info:
+        continue
+      file_name = file_info["name"]
+      file_name_no_ext = Path(file_name).stem
+      if is_file_in_dir(files, file_name):
+        media_links.append({
+          'link': ("{}/{}".format(folder, file_name)).replace("./static", ""),
+          'timestamp': None if "timestamp" not in file_info else file_info["timestamp"],
+          'caption': file_name_no_ext.replace("-"," ").title() if "caption" not in file_info else file_info["caption"],
+          'video': False if "video" not in file_info else file_info["video"]
+        })
+      elif "video" in file_info and file_info["video"]:
+        media_links.append({
+          'link': file_info["link"],
+          'timestamp': None if "timestamp" not in file_info else file_info["timestamp"],
+          'caption': file_name_no_ext.replace("-"," ").title() if "caption" not in file_info else file_info["caption"],
+          'video': True
+        })
+      file_names.add(file_name)
+  # Now add whatever is left over
+  for file in files:
+    file_name = os.path.basename(file)
+    file_name_no_ext = Path(file).stem
+    if file_name in file_names:
+      continue # skip files we've already done
+    media_links.append({
+      'link': ("{}/{}".format(folder, file_name)).replace("./static", ""),
+      'timestamp': None,
+      'caption': file_name_no_ext.replace("-"," ").title(),
+      'video': False # TODO - support finding video files
+    })
+  return media_links
+
+
+def init_gallery(gallery):
+  for _, sub_gallery in gallery["galleries"].items():
+    folder_to_search = "./static/gallery/{}".format(sub_gallery["folder"])
+    # Check if there is a `meta.json` file which provides ordering and captions
+    meta_file = None
+    if os.path.exists("{}/_meta.json".format(folder_to_search)):
+      with open("{}/_meta.json".format(folder_to_search), 'r') as f:
+        meta_file = json.load(f)
+    # Initialize the media links
+    if os.path.isdir(folder_to_search):
+      sub_gallery["media"] = init_media_links(folder_to_search, meta_file)
+
+
+def generate_gallery(gallery, out_path):
   # Read in the gallery templates
   master_template_file = ""
   with open('./scripts/gallery-updater/gallery-master.template', 'r', encoding='utf-8') as f:
@@ -128,48 +114,34 @@ def generate_gallery(title, description, game_name, gallery_names, out_path):
     gallery_template_file = f.read()
 
   # Replace title and description
-  master_template_file = master_template_file.replace("___TITLE___", title)
-  master_template_file = master_template_file.replace("___DESCRIPTION___", description)
-
-  # Generate image imports
-  # this checks all images in all galleries supplied
-  image_imports = []
-  image_idx = 0
-  for gallery_name in gallery_names:
-    gallery = galleryLinks[game_name]["galleries"][gallery_name]
-    for entry in gallery["media"]:
-      if not entry["video"]:
-        image_imports.append("import image{} from '/gallery/{}/{}';".format(image_idx, gallery["folder"], entry["fileName"]))
-        image_idx = image_idx + 1
-  master_template_file = master_template_file.replace("___IMAGE_IMPORTS___", "\n".join(image_imports))
+  master_template_file = master_template_file.replace("___TITLE___", gallery["metaTitle"])
+  master_template_file = master_template_file.replace("___DESCRIPTION___", gallery["metaDescription"])
 
   # Generate each (sub-)gallery
   galleries_text = ""
-  image_idx = 0
-  for gallery_name in gallery_names:
-    gallery = galleryLinks[game_name]["galleries"][gallery_name]
+  for _, sub_gallery in gallery["galleries"].items():
     # Generate the actual images, 3 per row
     row_count = 0
     gallery_items = ""
     current_gallery_content = gallery_template_file
     # Replace gallery title and description
-    current_gallery_content = current_gallery_content.replace("___TITLE___", "<i>{}</i> {}".format(galleryLinks[game_name]["name"], gallery["name"]))
-    current_gallery_content = current_gallery_content.replace("___DESCRIPTION___", gallery["description"])
-    for entry in gallery["media"]:
+    current_gallery_content = current_gallery_content.replace("___TITLE___", "<i>{}</i>".format(sub_gallery["name"]))
+    current_gallery_content = current_gallery_content.replace("___DESCRIPTION___", sub_gallery["description"])
+    for idx, media_entry in enumerate(sub_gallery["media"]):
       if row_count % 3 == 0:
         gallery_items = gallery_items + '            <div className="row center">\n'
       gallery_items = gallery_items + '              <div className="col col--4">\n'
-      if entry["video"]:
-        gallery_items = gallery_items + '                <iframe width="100%" height="300px" src="{}"></iframe>\n'.format(entry["link"])
+      if media_entry["video"]:
+        gallery_items = gallery_items + '                <ReactPlayer controls width=\'100%\' url="{}"></ReactPlayer>\n'.format(media_entry["link"])
       else:
-        gallery_items = gallery_items + '                <img loading="lazy" src={{image{}}} alt="{} - {}" />\n'.format(image_idx, entry["caption"], entry["timestamp"])
-      gallery_items = gallery_items + '                <blockquote>{}</blockquote>\n'.format(entry["caption"])
+        imgSrc = "useBaseUrl('{}')".format(media_entry["link"])
+        gallery_items = gallery_items + '                <img loading="lazy" src={{{}}} alt="{} - {}" />\n'.format(imgSrc, media_entry["caption"], media_entry["timestamp"])
+      if media_entry["caption"] != None:
+        gallery_items = gallery_items + '                <blockquote>{}</blockquote>\n'.format(media_entry["caption"])
       gallery_items = gallery_items + '              </div>\n'
       row_count = row_count + 1
-      if row_count % 3 == 0:
+      if row_count % 3 == 0 or idx == len(sub_gallery["media"]) - 1:
         gallery_items = gallery_items + '            </div>\n'
-      if not entry["video"]:
-        image_idx = image_idx + 1
     current_gallery_content = current_gallery_content.replace("___ENTRIES___", gallery_items)
     galleries_text = galleries_text + current_gallery_content
 
@@ -179,6 +151,10 @@ def generate_gallery(title, description, game_name, gallery_names, out_path):
   with open(out_path, 'w', encoding='utf-8') as f:
     f.write(master_template_file)
 
+# generate the gallery data
+init_gallery(galleryMap['jak1'])
+init_gallery(galleryMap['jak2'])
+
 # write the actual gallery pages
-generate_gallery("Jak and Daxter: The Precursor Legacy Galleries", "Jak and Daxter promo & development galleries", "jak1", ["promo", "dev"], "./src/pages/gallery/jak1.js")
-generate_gallery("Jak II Galleries", "Jak II promo & development galleries", "jak2", ["dev"], "./src/pages/gallery/jak2.js")
+generate_gallery(galleryMap['jak1'], "./src/pages/gallery/jak1.js")
+generate_gallery(galleryMap['jak2'], "./src/pages/gallery/jak2.js")
