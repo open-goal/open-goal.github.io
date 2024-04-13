d2ed511a8e1e75faae2247c013d72f3207b2fdc7
Tyler Wilding <xtvaser@gmail.com>
2024-04-12 23:57:51 -0400

    scripts: empty cells sometimes return `0` it would seem

--- scripts/progress-tracker/index.js
+++ scripts/progress-tracker/index.js
@@ -419,8 +419,8 @@ function auditProcess(gameName, pulls, issues) {
     if (gameName === "jak3") {
       // only jak 3 is in active development
       const assignment = getSheetAssignmentFromName(fileName);
-      if (assignment !== null && assignment !== undefined) {
-        console.log(`File: ${fileName} - Assignment: ${assignment}`);
+      if (assignment !== null && assignment !== undefined && assignment !== 0) {
+        // console.log(`File: ${fileName} - Assignment: ${assignment}`);
         const assignmentLenient = assignment.replace(" ", "").toLowerCase();
         if (assignmentLenient in sheetAssignmentMapping) {
           entry.assignedTo.sheet = sheetAssignmentMapping[assignmentLenient];
